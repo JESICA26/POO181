02bbdf65b1f551e1bb051d879fc63fe504df42ae
JESICA26 <133449756+JESICA26@users.noreply.github.com>
2023-08-02 16:08:41 -0500

    Actividad finalizada
    Actividad finalizada 9

--- Practica5/app.py
+++ Practica5/app.py
@@ -1,92 +1,92 @@
-#Importacion del framework
-from flask import Flask, render_template, request, redirect, url_for, flash
+# Importacion de framework -------------------------------------------
+from flask import Flask,render_template,request,redirect,url_for,flash
+# Importacion de MySQL con FLASK
 from flask_mysqldb import MySQL
 
-#Inicializacion del Servidor
-app=Flask(__name__)
+# Inicialización del APP ó servidor ----------------------------------
+app= Flask(__name__)
 
-#Configuracion de la conexion
+# Conexion de la base de datos ---------------------------------------
 app.config['MYSQL_HOST']='localhost'
-app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']=''
-app.config['MYSQL_DB']='dbflask'
+app.config['MYSQL_USER']='root' # Usuario de MySQL 
+app.config['MYSQL_PASSWORD']=''  # Contraseña MySQL
+app.config['MYSQL_DB']='dbflask'  # Nombre de la base de datos
 app.secret_key='mysecretkey'
-mysql=MySQL(app)
+mysql= MySQL(app)
 
-#Declaracion de la ruta http://localhost:5000
+# Declaración de las rutas hhtp://localhost:5000 ---------------------
+
+  # Ruta principal -------------
 @app.route('/')
 def index():
-    consulta= mysql.connect.cursor()
-    consulta.execute('select * from Albums')
-    conAlbums= consulta.fetchall()
-    #print(conAlbums)
-    return render_template('index.html', albums = conAlbums)
+    CC= mysql.connection.cursor()
+    CC.execute('select * from albums')
+    conAlbums= CC.fetchall()
+    print(conAlbums)
+    return render_template('index.html', listalbums=conAlbums)
 
-#Ruta http://localhost:5000/guardar tipo POST para insert
-@app.route('/guardar', methods=['POST'])
+# ruta http:localhost:500/guardar tipo POST para Insert
+@app.route('/guardar', methods=['POST'] )
 def guardar():
     if request.method == 'POST':
+    #Pasamos a variables el contenido de los input
+        Vtitulo= request.form['txtTitulo']
+        Vartista= request.form['txtArtista']
+        Vanio= request.form['txtAnio']
+        print(Vtitulo,Vartista,Vanio)
         
-        #pasamos a variables el contenido de los input 
-        vtitulo= request.form['txtTitulo']
-        vartista= request.form['txtArtista']
-        vanio= request.form['txtAnio']
-        #print(titulo,artista,anio)
-        
-        #Conectar y ejecutar el insert
-        cs = mysql.connection.cursor()
-        cs.execute('insert into Albums(titulo,artista,anio) values (%s,%s,%s)', (vtitulo,vartista,vanio))
+        if Vtitulo == "" or Vartista== "" or Vanio == "":
+            flash('No puedes enviar campos vacios')
+            return redirect(url_for('index'))
+        else:
+            #Conectar y ejecutar el insert
+            CS = mysql.connection.cursor()
+            CS.execute('insert into albums(titulo,artista,anio) values (%s,%s,%s)',(Vtitulo,Vartista,Vanio))
+            mysql.connection.commit()
+            flash('El album fue agregado correctamente')
+            return redirect(url_for('index'))
+
+@app.route('/editar/<id>')
+def editar(id):
+    cursorId = mysql.connection.cursor()
+    cursorId.execute('select * from albums where id= %s',(id,))
+    consulId = cursorId.fetchone()
+    return render_template('editarAlbum.html', album = consulId)
+
+@app.route('/actualizar/<id>', methods=['POST'])
+def actualizar(id):
+    if request.method == 'POST':
+        #Pasamos a variables el contenido de los input
+        Vartitulo= request.form['txtTitulo']
+        Vartista= request.form['txtArtista']
+        Varanio= request.form['txtAnio']
+    
+        curAct = mysql.connection.cursor()
+        curAct.execute('update albums set titulo= %s, artista= %s, anio= %s where id= %s', (Vartitulo,Vartista,Varanio,id))
         mysql.connection.commit()
         
-    flash('El album fue agregado correctamente')
-    return redirect(url_for('index'))
-
-@app.route('/editar/<string:id>')
-def editar(id):
-    cursoId=mysql.connection.cursor()
-    cursoId.execute('select * from Albums where id= %s',(id))
-    consulId= cursoId.fetchone()
-    print(consulId)
-    flash('El album fue actualizado correctamente')
-    return render_template('editarAlbum.html',album=consulId)
-    
-@app.route('/update/<id>', methods=['POST'])
-def update(id):
-   if request.method == 'POST':
-    varTitulo= request.form['txtTitulo']
-    varArtista= request.form['txtArtista']
-    varAnio= request.form['txtAnio']
-    curAct= mysql.connection.cursor()
-    curAct.execute('update Albums set titulo= %s,artista= %s, anio= %s  where id= %s',(varTitulo,varArtista,varAnio,id))
-    mysql.connection.commit()
-    
-    
-    flash('Se actualizo el Album'+varTitulo)    
+    flash('El album se actualizo correctamente')
     return redirect(url_for('index'))
 
 @app.route('/eliminar/<id>')
-def eliminar(id):
-    cursorId=mysql.connection.cursor()
-    cursorId.execute('select * from Albums where id=%s',(id,))
-    consId = cursorId.fetchone()
-    return render_template('borraralbum.html', album= consId)
+def borrar(id):
+    cursorId = mysql.connection.cursor()
+    cursorId.execute('select * from albums where id= %s',(id,))
+    consulId = cursorId.fetchone()
+    return render_template('eliminarAlbum.html', album = consulId)
 
-@app.route('/delete/<id>',methods=['POST'])
-def actualizar(id):
+@app.route('/borrar/<id>', methods=['POST'])
+def eliminar(id):
     if request.method == 'POST':
-        varTitulo = request.form['txtTitulo']
-        
-        curAct = mysql.connection.cursor()
-        curAct.execute('delete from Albums where id=%s',(id))
+
+        cursorAct = mysql.connection.cursor()
+        cursorAct.execute('delete from albums where id= %s',(id,))
         mysql.connection.commit()
-    flash('Se borró el Album '+varTitulo)
+        
+    flash('Se elimino el album correctamente')
     return redirect(url_for('index'))
 
-
-
-
-    
-#Ejecucion de nuestro programa
+# Ejecución de Servidor en el Puerto 5000 ---------------------------- 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000,debug=True)
     
